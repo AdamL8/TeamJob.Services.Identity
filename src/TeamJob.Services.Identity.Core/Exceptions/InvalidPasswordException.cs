545a1ffe049ae017377c071fe75ded87216be299
AdamL8 <AdamL8@users.noreply.github.com>
2020-07-22 20:55:56 -0400

    Standardize exception code so that the UI can apply translation table (#7)

--- src/TeamJob.Services.Identity.Core/Exceptions/InvalidPasswordException.cs
+++ src/TeamJob.Services.Identity.Core/Exceptions/InvalidPasswordException.cs
@@ -6,7 +6,7 @@ namespace TeamJob.Services.Identity.Core.Exceptions
 {
     public class InvalidPasswordException : DomainException
     {
-        public override string Code { get; } = "invalid_password";
+        public override string Code { get; } = "service.identity.exception.invalid_password";
 
         public InvalidPasswordException()
             : base($"Invalid password.")
